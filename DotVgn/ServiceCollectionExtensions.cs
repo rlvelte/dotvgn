26ee0543f461f5e7fd843a62be94aba98ef68994
Robin L. Velte <robin.leandro.velte@icloud.com>
2026-01-09 00:34:34 +0100

    refactor: change service collection extensions

--- DotVgn/ServiceCollectionExtensions.cs
+++ DotVgn/ServiceCollectionExtensions.cs
@@ -14,6 +14,10 @@ namespace DotVgn;
 /// Extension methods to register DotVGN client with DI.
 /// </summary>
 public static class ServiceCollectionExtensions {
+    private const string DefaultSectionName = "DotVgn";
+    private const string UserAgentHeader = "DotVgn/1.0 (+https://github.com/rlvelte/DotVgn)";
+    private const string AcceptHeader = "application/json";
+
     /// <param name="services">The service collection.</param>
     extension(IServiceCollection services) {
         /// <summary>
@@ -25,22 +29,9 @@ public static class ServiceCollectionExtensions {
             ArgumentNullException.ThrowIfNull(services);
             ArgumentNullException.ThrowIfNull(configure);
 
-            services.AddSingleton<IConfigureOptions<DotVgnClientOptions>>(sp => new ConfigureOptions<DotVgnClientOptions>(opt => configure(sp, opt)));
-            services.AddOptions<DotVgnClientOptions>()
-                    .Validate(o => o.BaseUri.IsAbsoluteUri, "DotVgn: BaseUri must be an absolute URI.")
-                    .Validate(o => o.MaxParallelRequestsForBatch >= 1, "DotVgn: MaxParallelRequestsForBatch must be >= 1.")
-                    .ValidateOnStart();
-            
-            services.AddHttpClient<DotVgnClient>((sp, http) => { 
-                        var options = sp.GetRequiredService<IOptions<DotVgnClientOptions>>().Value;
-                        http.BaseAddress = options.BaseUri;
-                        http.DefaultRequestHeaders.UserAgent.ParseAdd("DotVgn/1.0 (+https://github.com/rlvelte/DotVgn)");
-                        http.DefaultRequestHeaders.Accept.ParseAdd("application/json"); 
-                    }).ConfigurePrimaryHttpMessageHandler(() => new SocketsHttpHandler {
-                        AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
-                    }).AddPolicyHandler((_, _) => BuildResiliencePolicy());
-            
-            services.AddTransient<IDotVgnClient>(sp => sp.GetRequiredService<DotVgnClient>());
+            ConfigureOptions(services, sp => opt => configure(sp, opt));
+            RegisterHttpClient(services);
+        
             return services;
         }
 
@@ -50,28 +41,14 @@ public static class ServiceCollectionExtensions {
         /// <param name="configuration">The application configuration.</param>
         /// <param name="sectionName">Optional section name. Defaults to "DotVgn".</param>
         /// <returns>The service collection.</returns>
-        public IServiceCollection AddDotVgnClient(IConfiguration configuration, string sectionName = "DotVgn") {
+        public IServiceCollection AddDotVgnClient(IConfiguration configuration, string sectionName = DefaultSectionName) {
             ArgumentNullException.ThrowIfNull(services);
             ArgumentNullException.ThrowIfNull(configuration);
 
             var section = configuration.GetSection(sectionName);
-
-            services.AddSingleton<IConfigureOptions<DotVgnClientOptions>>(_ => new ConfigureOptions<DotVgnClientOptions>(opt => section.Bind(opt)));
-            services.AddOptions<DotVgnClientOptions>()
-                    .Validate(o => o.BaseUri.IsAbsoluteUri, "DotVgn: BaseUri must be an absolute URI.")
-                    .Validate(o => o.MaxParallelRequestsForBatch >= 1, "DotVgn: MaxParallelRequestsForBatch must be >= 1.")
-                    .ValidateOnStart();
-            
-            services.AddHttpClient<DotVgnClient>((sp, http) => {
-                        var options = sp.GetRequiredService<IOptions<DotVgnClientOptions>>().Value;
-                        http.BaseAddress = options.BaseUri;
-                        http.DefaultRequestHeaders.UserAgent.ParseAdd("DotVgn/1.0 (+https://github.com/rlvelte/DotVgn)");
-                        http.DefaultRequestHeaders.Accept.ParseAdd("application/json");
-                    }).ConfigurePrimaryHttpMessageHandler(() => new SocketsHttpHandler {
-                        AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
-                    }).AddPolicyHandler((_, _) => BuildResiliencePolicy());
-
-            services.AddTransient<IDotVgnClient>(sp => sp.GetRequiredService<DotVgnClient>());
+            ConfigureOptions(services, _ => section.Bind);
+            RegisterHttpClient(services);
+        
             return services;
         }
 
@@ -86,57 +63,139 @@ public static class ServiceCollectionExtensions {
 
             services.AddOptions<DotVgnClientOptions>()
                     .Configure(configure)
-                    .Validate(o => o.BaseUri.IsAbsoluteUri, "DotVgn: BaseUri must be an absolute URI.")
-                    .Validate(o => o.MaxParallelRequestsForBatch >= 1, "DotVgn: MaxParallelRequestsForBatch must be >= 1.")
-                    .ValidateOnStart();
+                    .ValidateOptions();
 
-            services.AddHttpClient<DotVgnClient>((sp, http) => {
-                        var options = sp.GetRequiredService<IOptions<DotVgnClientOptions>>().Value;
-                        http.BaseAddress = options.BaseUri;
-                        http.DefaultRequestHeaders.UserAgent.ParseAdd("DotVgn/1.0 (+https://github.com/rlvelte/DotVgn)");
-                        http.DefaultRequestHeaders.Accept.ParseAdd("application/json");
-                    }).ConfigurePrimaryHttpMessageHandler(() => new SocketsHttpHandler {
-                        AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
-                    }).AddPolicyHandler((_, _) => BuildResiliencePolicy());
-
-            services.AddTransient<IDotVgnClient>(sp => sp.GetRequiredService<DotVgnClient>());
+            RegisterHttpClient(services);
+        
             return services;
         }
     }
-    
-    private static IAsyncPolicy<HttpResponseMessage> BuildResiliencePolicy() {
-        var jitter = new Random();
-        var timeout = Policy.TimeoutAsync<HttpResponseMessage>(TimeSpan.FromSeconds(10));
 
-        var retry = HttpPolicyExtensions
+    private static void ConfigureOptions(IServiceCollection services, Func<IServiceProvider, Action<DotVgnClientOptions>> configureFactory) {
+        services.AddSingleton<IConfigureOptions<DotVgnClientOptions>>(
+            sp => new ConfigureOptions<DotVgnClientOptions>(configureFactory(sp)));
+        
+        services.AddOptions<DotVgnClientOptions>()
+            .ValidateOptions();
+    }
+
+    private static OptionsBuilder<DotVgnClientOptions> ValidateOptions(this OptionsBuilder<DotVgnClientOptions> builder) {
+        return builder
+            .Validate(o => o.BaseUri.IsAbsoluteUri, 
+                "DotVgn: BaseUri must be an absolute URI.")
+            .Validate(o => o.MaxParallelRequestsForBatch >= 1, 
+                "DotVgn: MaxParallelRequestsForBatch must be >= 1.")
+            .ValidateOnStart();
+    }
+
+    private static void RegisterHttpClient(IServiceCollection services) {
+        services.AddHttpClient<DotVgnClient>(ConfigureHttpClient)
+            .ConfigurePrimaryHttpMessageHandler(CreateHttpMessageHandler)
+            .AddPolicyHandler((_, _) => ResiliencePolicyFactory.BuildPolicy());
+
+        services.AddTransient<IDotVgnClient>(sp => sp.GetRequiredService<DotVgnClient>());
+    }
+
+    private static void ConfigureHttpClient(IServiceProvider sp, HttpClient http) {
+        var options = sp.GetRequiredService<IOptions<DotVgnClientOptions>>().Value;
+        
+        http.BaseAddress = options.BaseUri;
+        http.DefaultRequestHeaders.UserAgent.ParseAdd(UserAgentHeader);
+        http.DefaultRequestHeaders.Accept.ParseAdd(AcceptHeader);
+    }
+
+    private static HttpMessageHandler CreateHttpMessageHandler() {
+        return new SocketsHttpHandler {
+            AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+        };
+    }
+}
+
+/// <summary>
+/// Factory for creating resilience policies for HTTP requests.
+/// </summary>
+internal static class ResiliencePolicyFactory {
+    private const int TimeoutSeconds = 10;
+    private const int RetryCount = 4;
+    private const int MaxRetryAfterSeconds = 30;
+    private const int MaxBackoffSeconds = 20;
+    private const int MaxJitterMilliseconds = 250;
+    private const int CircuitBreakerThreshold = 5;
+    private const int CircuitBreakerDurationSeconds = 30;
+
+    public static IAsyncPolicy<HttpResponseMessage> BuildPolicy() {
+        var timeout = CreateTimeoutPolicy();
+        var retry = CreateRetryPolicy();
+        var circuitBreaker = CreateCircuitBreakerPolicy();
+
+        return Policy.WrapAsync(retry, circuitBreaker, timeout);
+    }
+
+    private static IAsyncPolicy<HttpResponseMessage> CreateTimeoutPolicy() {
+        return Policy.TimeoutAsync<HttpResponseMessage>(TimeSpan.FromSeconds(TimeoutSeconds));
+    }
+
+    private static IAsyncPolicy<HttpResponseMessage> CreateRetryPolicy() {
+        var jitter = new Random();
+
+        return HttpPolicyExtensions
             .HandleTransientHttpError()
             .OrResult(r => r.StatusCode is (HttpStatusCode)429 or HttpStatusCode.ServiceUnavailable)
-            .WaitAndRetryAsync(retryCount: 4, (retryAttempt, outcome, _) => {
-                                   var retryAfter = outcome?.Result?.Headers?.RetryAfter;
-                                   if (retryAfter != null) {
-                                       var ra = retryAfter.Delta ?? (retryAfter.Date.HasValue ? retryAfter.Date.Value - DateTimeOffset.UtcNow : null);
-                                       if (ra.HasValue && ra.Value > TimeSpan.Zero) {
-                                           var capped = ra.Value > TimeSpan.FromSeconds(30) ? TimeSpan.FromSeconds(30) : ra.Value;
-                                           return capped;
-                                       }
-                                   }
-
-                                   var backoff = TimeSpan.FromSeconds(Math.Pow(2, retryAttempt));
-                                   var jitterMs = jitter.Next(0, 250);
-                                   var delay = backoff + TimeSpan.FromMilliseconds(jitterMs);
-                                   
-                                   return delay > TimeSpan.FromSeconds(20) ? TimeSpan.FromSeconds(20) : delay;
-                               }, (_, _, _, _) => Task.CompletedTask
+            .WaitAndRetryAsync(
+                retryCount: RetryCount,
+                sleepDurationProvider: (retryAttempt, outcome, _) => 
+                    CalculateRetryDelay(retryAttempt, outcome, jitter),
+                onRetryAsync: (_, _, _, _) => Task.CompletedTask
             );
+    }
 
-        var circuitBreaker = HttpPolicyExtensions
+    private static TimeSpan CalculateRetryDelay(int retryAttempt, DelegateResult<HttpResponseMessage> outcome, Random jitter) {
+        var retryAfterDelay = TryGetRetryAfterDelay(outcome?.Result);
+        if (retryAfterDelay.HasValue)
+        {
+            return retryAfterDelay.Value;
+        }
+
+        var backoff = TimeSpan.FromSeconds(Math.Pow(2, retryAttempt));
+        var jitterMs = jitter.Next(0, MaxJitterMilliseconds);
+        var delay = backoff + TimeSpan.FromMilliseconds(jitterMs);
+
+        return delay > TimeSpan.FromSeconds(MaxBackoffSeconds) 
+            ? TimeSpan.FromSeconds(MaxBackoffSeconds) 
+            : delay;
+    }
+
+    private static TimeSpan? TryGetRetryAfterDelay(HttpResponseMessage? response) {
+        var retryAfter = response?.Headers?.RetryAfter;
+        if (retryAfter == null)
+        {
+            return null;
+        }
+
+        var delay = retryAfter.Delta ?? 
+            (retryAfter.Date.HasValue 
+                ? retryAfter.Date.Value - DateTimeOffset.UtcNow 
+                : null);
+
+        if (!delay.HasValue || delay.Value <= TimeSpan.Zero)
+        {
+            return null;
+        }
+
+        var cappedDelay = delay.Value > TimeSpan.FromSeconds(MaxRetryAfterSeconds)
+            ? TimeSpan.FromSeconds(MaxRetryAfterSeconds)
+            : delay.Value;
+
+        return cappedDelay;
+    }
+
+    private static IAsyncPolicy<HttpResponseMessage> CreateCircuitBreakerPolicy() {
+        return HttpPolicyExtensions
             .HandleTransientHttpError()
             .OrResult(r => r.StatusCode == (HttpStatusCode)429)
             .CircuitBreakerAsync(
-                handledEventsAllowedBeforeBreaking: 5,
-                durationOfBreak: TimeSpan.FromSeconds(30)
+                handledEventsAllowedBeforeBreaking: CircuitBreakerThreshold,
+                durationOfBreak: TimeSpan.FromSeconds(CircuitBreakerDurationSeconds)
             );
-
-        return Policy.WrapAsync(retry, circuitBreaker, timeout);
     }
 }
--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Pragmatic client to query the VGN/VAG public transport API for:
 > This package was extracted from a personal project. Ongoing development is driven by my perceived needs rather than a fixed roadmap. Contributions, improvements, and forks are very welcome.
 
 ## Install
-The easiest way to get started is to install the package from GitHub Packages:
+The easiest way to get started is to install the package from [GitHub Packages](https://docs.github.com/de/packages/working-with-a-github-packages-registry/working-with-the-nuget-registry):
 ```
 dotnet add package dotvgn
 ```
